773ea07a6eec1b56c2ea13bb617345f064529a03
Dominik Honnef <dominik@honnef.co>
2016-01-03 06:33:55 +0100

    raster: add a test for multi-page streams

--- raster/decode_test.go
+++ raster/decode_test.go
@@ -2,6 +2,7 @@ package raster
 
 import (
 	"fmt"
+	"io"
 	"os"
 	"testing"
 
@@ -29,4 +30,36 @@ func TestDecode(t *testing.T) {
 		t.Fatal(err)
 	}
 	fmt.Printf("%s", b)
+}
+
+func TestDecodeMultiplePages(t *testing.T) {
+	f, err := os.Open("testdata/two_pages")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer f.Close()
+	d, err := NewDecoder(f)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	i := 0
+	for {
+		p, err := d.NextPage()
+		if err == io.EOF {
+			t.Logf("read %d pages", i)
+			break
+		}
+		i++
+		if err != nil {
+			t.Fatal(err)
+		}
+		spew.Dump(p.Header)
+		b := make([]byte, p.TotalSize())
+		err = p.ReadAll(b)
+		if err != nil {
+			t.Fatal(err)
+		}
+		fmt.Printf("%s", b)
+	}
 }
