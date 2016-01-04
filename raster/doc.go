b3101ba1f5f4db68a1506c6dea1fc851578280c6
Dominik Honnef <dominik@honnef.co>
2016-01-04 09:57:48 +0100

    raster: add package-level documentation

---
+++ raster/doc.go
@@ -0,0 +1,7 @@
+// Package raster implements a decoder for the CUPS raster format. It
+// provides functions for decoding a CUPS raster stream line-wise or
+// page-wise.
+//
+// For a list of currently supported color spaces and bit depths, see
+// the documentation of Page.ParseColors.
+package raster
