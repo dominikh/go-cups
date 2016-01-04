fdf0bb27d05eaf008923e693900f294ec041fc7c
Dominik Honnef <dominik@honnef.co>
2016-01-04 15:00:14 +0100

    raster: improve test suite

--- raster/decode_test.go
+++ raster/decode_test.go
@@ -1,21 +1,94 @@
 package raster
 
 import (
+	"compress/gzip"
 	"io"
 	"os"
 	"testing"
 )
 
-func open(s string, t *testing.T) *os.File {
-	f, err := os.Open(s)
+type file struct {
+	f *os.File
+	r *gzip.Reader
+}
+
+func (f file) Read(b []byte) (int, error) {
+	return f.r.Read(b)
+}
+
+func (f file) Close() error {
+	return f.f.Close()
+}
+
+func open(s string, t *testing.T) io.ReadCloser {
+	f, err := os.Open("testdata/" + s + ".gz")
 	if err != nil {
 		t.Fatal(err)
 	}
-	return f
+
+	gz, err := gzip.NewReader(f)
+	if err != nil {
+		t.Fatal(err)
+	}
+	return file{f, gz}
+}
+
+func TestDecode(t *testing.T) {
+	const width = 633
+	const height = 633
+	const size = width * height
+	var tests = []struct {
+		file      string
+		openErr   error
+		parseErr  error
+		numColors int
+	}{
+		// The width of the image is 633 pixels. However, we store 8
+		// pixels in one byte, so the byte for the last pixel in each
+		// line contains 7 extra bits.
+		{"gradient_chunked_k_1_1", nil, nil, size + 7*height},
+		{"gradient_chunked_k_8_8", nil, nil, size},
+		{"gradient_chunked_cmyk_8_32", nil, nil, size},
+		{"gradient_chunked_cmyk_1_4", nil, ErrUnsupported, size},
+		{"garbage", ErrUnknownVersion, nil, 1e4},
+	}
+
+	for _, tt := range tests {
+		f := open(tt.file, t)
+		defer f.Close()
+		d, err := NewDecoder(f)
+		if err != tt.openErr {
+			t.Errorf("NewDecoder(%s): got err %v, want %v", tt.file, err, tt.openErr)
+		}
+		if err != nil {
+			continue
+		}
+		p, err := d.NextPage()
+		if err != nil {
+			t.Errorf("%s: d.NextPage(): got err %v, want nil", tt.file, err)
+			continue
+		}
+		b := make([]byte, p.TotalSize())
+		err = p.ReadAll(b)
+		if err != nil {
+			t.Errorf("%s: p.ReadAll(): got err %v, want nil", tt.file, err)
+			continue
+		}
+		colors, err := p.ParseColors(b)
+		if err != tt.parseErr {
+			t.Errorf("%s: p.ParseColors(): got err %v, want %v", tt.file, err, tt.parseErr)
+		}
+		if err != nil {
+			continue
+		}
+		if len(colors) != tt.numColors {
+			t.Errorf("%s: parsed %d colors, want %d", tt.file, len(colors), tt.numColors)
+		}
+	}
 }
 
 func TestDecodeMultiplePages(t *testing.T) {
-	f := open("testdata/two_pages", t)
+	f := open("two_pages", t)
 	defer f.Close()
 	d, err := NewDecoder(f)
 	if err != nil {
@@ -41,7 +114,7 @@ func TestDecodeMultiplePages(t *testing.T) {
 }
 
 func TestDecodeTruncatedLine(t *testing.T) {
-	f := open("testdata/raster_truncated", t)
+	f := open("raster_truncated", t)
 	defer f.Close()
 	d, err := NewDecoder(f)
 	if err != nil {
@@ -71,7 +144,7 @@ func TestDecodeTruncatedLine(t *testing.T) {
 
 func TestDecodeMissingLine(t *testing.T) {
 	t.Skip("TODO(dh): provide fixture")
-	f := open("testdata/raster_missing_line", t)
+	f := open("raster_missing_line", t)
 	defer f.Close()
 	d, err := NewDecoder(f)
 	if err != nil {
@@ -89,7 +162,7 @@ func TestDecodeMissingLine(t *testing.T) {
 }
 
 func TestDecodeTruncatedHeader(t *testing.T) {
-	f := open("testdata/truncated_header", t)
+	f := open("truncated_header", t)
 	defer f.Close()
 	d, err := NewDecoder(f)
 	if err != nil {
